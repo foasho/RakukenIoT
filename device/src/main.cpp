277a3be8c6006f7613b2cf768d78b8d3a58d0861
foasho <foa.sho0111@outlook.jp>
2022-12-22 16:53:39 +0900

    update: 重さ処理の調整

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -7,24 +7,39 @@
 
 #define BANDRATE 115200
 #define WEIGHT_THRESHOLD 20
+RTC_DATA_ATTR int BOOT_COUNTER = 0;//起動回数
+
+void autoCalibration();
+void sleepMode();
+void forceSleepMode();
 
 void setup() {
     Serial.begin(BANDRATE);
-    delay(1000); // 安定化のためのDelay
+    delay(500);
     Serial.println("--- SETUP Start ---");
+    Serial.print("BOOT_COUNTER: ");
+    Serial.println(BOOT_COUNTER);
 
     // 設定ファイルを読み取る
     loadConfigFile();
 
     // 各IOをセットアップする
     setupIO();
+    //自動キャリブレーションをコメントアウト※測定できないときがあったため
+    // if (BOOT_COUNTER == 0 || BOOT_COUNTER % autoCalibrationNum == 0){
+    //     waitNotWeight(WEIGHT_THRESHOLD, 12000);
+    //     autoCalibration();
+    // }
+    BOOT_COUNTER = BOOT_COUNTER + 1;
+    delay(50);
+    if (kg < WEIGHT_THRESHOLD) sleepMode();
 
     // WiFiに接続する
     int rssi = connectWiFi(saveData.ssid, saveData.password);
     if (rssi == 0){
         Serial.println("--- NOT CONNECT WiFI ---");
         delay(3000);
-        ESP.restart();
+        sleepMode();
     }
 
     
@@ -60,9 +75,13 @@ void loop() {
             Serial.println("SUCCESS SEND API");
             if (isHttps){
                 // LINEBotに通知を送る
-                
+                sendLineBot(
+                    saveData.endpoint,
+                    saveData.access_token,
+                    kg
+                );
             }
-            forceDeepSleep(60 * 3); // 強制3分スリープ
+            forceSleepMode(); // 強制3分スリープ
         }
         else {
             orderBuzzer(1500); //失敗時は長くブザーを鳴らす
@@ -73,4 +92,45 @@ void loop() {
         Serial.println("NOT FOUND WEIGHT");
         deepSleep(86400*30);
     }
+}
+
+void autoCalibration(){
+    Serial.println("## Start Auto Calibration");
+    zero_factor = scale.read_average();
+    Serial.print("ZeroFacror: ");
+    Serial.println(zero_factor);
+    scale.set_offset(zero_factor);
+    while(true){
+        delay(100);
+        kg = getKg();
+        Serial.print("KG: ");
+        Serial.println(kg);
+        Serial.print("Calibration: ");
+        Serial.println(calibration_factor);
+        if (kg > 0.1){
+            calibration_factor = calibration_factor - 10;
+            scale.set_scale(calibration_factor);
+        }
+        else if (kg < -0.1) {
+            calibration_factor = calibration_factor + 10;
+            scale.set_scale(calibration_factor);
+        }
+        else {
+            break;
+        }
+        delay(100);
+    }
+    Serial.println("## Complete Auto Calibration");
+}
+
+void sleepMode(){
+    Serial.println("Start Deep Sleep");
+    Serial.flush();
+    deepSleep(86400 * 30);
+};
+
+void forceSleepMode(){
+    Serial.println("3Minutes Force Deep Sleep");
+    forceDeepSleep(60 * 3);
+    Serial.flush();
 }
